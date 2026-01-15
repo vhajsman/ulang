78618083f4e028788953eb253ac33a6047344c05
vhajsman <1816.cookie1816@gmail.com>
2026-01-15 18:30:11 +0100

    add initial bytecode defs

---
+++ src/common/bytecode.hpp
@@ -0,0 +1,71 @@
+#ifndef __ULANG_COM_BYTECODE_H
+#define __ULANG_COM_BYTECODE_H
+
+#include <cstdint> // uint8_t
+#include <static_cast>
+
+#define ULANG_OP_COUNT 2
+#define ULANG_OP_MAX_DATA_SZ 16
+
+namespace ULang {
+    enum class OperandType: uint8_t {
+        OP_NULL         = 0b0000,    // instruction has no operand
+        OP_IMMEDIATE    = 0b0001,    // immediate value
+        OP_REFERENCE    = 0b0010,    // address in virtual memory
+        OP_CONSTANT     = 0b0100,    // constant from the pool
+        OP_REGISTER     = 0b1000     // internal register
+    };
+
+    typedef uint8_t operandMeta_t;
+
+    struct Operand {
+        ULang::OperandMeta_t raw_meta;
+        uint8_t* value;
+
+        /**
+         * @brief Get operand type
+         * 
+         * @return ULang::OperandType 
+         */
+        ULang::OperandType getType() const { 
+            return static_cast<ULang::OperandType>((raw_meta & 0b11110000) >> 4); 
+        }
+
+        /**
+         * @brief Get the size of operand data
+         * 
+         * @return size_t 
+         */
+        size_t getDataSz() const { 
+            return raw_meta & 0b00001111; 
+        }
+    };
+
+    enum class Opcode : uint8_t {
+        NOP,
+        PUSH,
+        POP,
+        ADD,
+        SUB,
+        MUL,
+        DIV,
+        LOAD,
+        STORE,
+        JMP,
+        JZ,
+        CALL,
+        RET,
+        HALT
+    };
+
+    struct Instruction {
+        Opcode opcode;
+        Operand opA; Operand opB;
+
+        size_t calcTotalSz() const {
+            return sizeof(opcode) + sizeof(operandMeta_t) * 2 + opA.getDataSz() + opB.getDataSz();
+        }
+    };
+};
+
+#endif
